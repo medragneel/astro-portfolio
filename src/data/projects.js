b27bb0995478a65ce7d4587e12db8231ab171e4d
mshadow <medmystgon@gmail.com>
2023-01-27 11:23:47 +0100

    v-0.1

---
+++ src/data/projects.js
@@ -0,0 +1,12 @@
+
+
+const projects = [
+    {
+        "id": 1,
+        "name": "Pixe",
+        "desc": "a pixe.la cli created in python",
+        "link": "https://github.com/medragneel/Pixe",
+        "img": "../../public/images/pixe.png",
+    },
+]
+
